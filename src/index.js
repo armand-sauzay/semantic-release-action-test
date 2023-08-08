8cf4720563c594498503cbccb6a1d70ac5349315
Armand Sauzay <35524799+armand-sauzay@users.noreply.github.com>
2023-08-08 03:27:43 +0000

    fix: wip

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import semanticRelease from 'semantic-release';
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    // Configuration for semantic-release
+    const config = {
+      branches: ['main'],
+      plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/github'
+      ],
+      ci: true,
+    };
+
+    // Execute semantic-release
+    const result = await semanticRelease(config);
+
+    if (result) {
+      const { lastRelease, commits, nextRelease, releases } = result;
+
+      // Example: log the new version
+      console.log(`Released version: ${nextRelease.version}`);
+
+      // Set outputs for further steps in your workflow, if necessary
+      core.setOutput('version', nextRelease.version);
+    } else {
+      console.log('No release published.');
+    }
+  } catch (error) {
+    core.setFailed(`Action failed with error: ${error}`);
+  }
 }
+
+run();
